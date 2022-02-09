ed6211255b21971ae7378fbbd607242bd6245a3d
Andrés David Hurtado Fernández <OsiNubis99@PM.me>
2022-02-08 22:00:06 -0400

    feat: requeriments added

--- config/env.py
+++ config/env.py
@@ -1,10 +1,9 @@
 import os
-
 if (os.getenv('db_url', None) == None):
 	from dotenv import load_dotenv
 	load_dotenv()
 
 url = os.getenv('HEROKU_SERVER_URL')
-bd_url = os.getenv('HEROKU_POSTGRESQL_URL')
+bd_url = os.getenv('DB_URL')
 token = os.getenv('TELEGRAM_TOKEN')
 port = os.getenv('PORT')
