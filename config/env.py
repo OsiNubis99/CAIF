aa5e9694a90505cc06343a0ff796b59b9d2af8d5
Andrés David Hurtado Fernández <OsiNubis99@PM.me>
2022-02-11 12:10:43 -0400

    feat: Server configuration

--- config/env.py
+++ config/env.py
@@ -3,8 +3,8 @@ if (os.getenv('db_url', None) == None):
 	from dotenv import load_dotenv
 	load_dotenv()
 
-url = os.getenv('HEROKU_SERVER_URL')
+url = os.getenv('HEROKU_SERVER_URL', 'localhost')
 db_url = os.getenv('DB_URL')
 db_name = os.getenv('DB_NAME')
 token = os.getenv('TELEGRAM_TOKEN')
-port = os.getenv('PORT')
+port = os.getenv('PORT', 3000)
